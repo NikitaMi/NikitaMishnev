44fb9aec153b56beb0d54f27f62a7ee4b411e10f
Кристина <nik-1995.95@mail.ru>
2018-10-08 10:00:00 +0700

    Пересечение без оптимизации

---
+++ Shapes/src/ru/nikita/mishnev/shapes/Square.java
@@ -0,0 +1,14 @@
+package ru.nikita.mishnev.shapes;
+
+public class Square extends Shape {
+    private double side;
+
+    public Square (double side){
+        this.side = side;
+    }
+
+    double getWidth(){
+        return 0;
+    }
+
+}
